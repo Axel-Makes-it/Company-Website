97c5de2fe202e6fc63201c3772af522cbb2e358b
Axel Ivan Santiago <axelivanlux@gmail.com>
2023-11-20 00:31:47 -0500

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,17 +6,17 @@ function Nav() {
   return (
     <div className="nav__container">
       <div className="nav">
-        <ul className="nav-ul-one">
+        <ul id="hide" className="nav-ul-one">
           <li>
-            <span>Home</span>
+            <span id="home">Home</span>
           </li>
           <li>
-            <span>
+            <span id="Solutions">
               Solutions <div id="plus">+</div>
             </span>
           </li>
           <li>
-            <span>Pricing</span>
+            <span id="Pricing">Pricing</span>
           </li>
         </ul>
         <div className="logo">
